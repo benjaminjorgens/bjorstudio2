ae20c7e0117bd8b2b7f53bfcda389ffdb4577b59
benjaminjorgens <ben.jorgens@gmail.com>
2026-03-21 21:38:46 -0400

    Add files via upload

---
+++ divider_data.js
@@ -0,0 +1,6 @@
+const DIVIDERS = {
+  "divider1.jpg": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAA...",
+  "divider2.jpg": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAA...",
+  "divider3.jpg": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAA...",
+  "divider4.jpg": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAA...",
+};
